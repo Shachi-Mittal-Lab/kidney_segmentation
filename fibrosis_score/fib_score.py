31664d965e6deb3134a311a62162815b19ee4d82
Rachel Ware <riware@uw.edu>
2024-09-13 12:08:28 -0700

    add comments to fibscore pipeline

--- fibrosis_score/fib_score.py
+++ fibrosis_score/fib_score.py
@@ -44,24 +44,24 @@ weights_path = "/home/riware/Documents/MittalLab/kidney_project/fibrosis_score/o
 
 ###########
 
-# convert ndpi to zarr array including* 1e7 levels 40x, 20x, 10x, 5x
+# convert ndpi to zarr array levels 40x, 20x, 10x, 5x
 for i in range(0, 4):
-    voxel_size = Coordinate(
-        int(1 / 44114.0 * 1e7) * 2**i, int(1 / 44114.0 * 1e7) * 2**i
-    )
-    # dask_array, x_res, y_res, units = openndpi(ndpi_path,i)
-    float_arr = open_ds(zarr_float_path / "raw" / f"s{i}")
+    # open the ndpi with openslide for info and tifffile as zarr
+    dask_array, x_res, y_res, units = openndpi(ndpi_path,i)
+    # define units in nm since native is cm and then you get a (0,0) res
     units = ("nm", "nm")
-    # voxel_size = (1/x_res * 2 ** i , 1/y_res * 2 ** i)
-    # raw = prepare_ds( zarr_path / "raw" / f"s{i}", dask_array.shape, offset, voxel_size, axis_names, units, dtype=np.uint8)
-    # raw = prepare_ds( zarr_path / "raw" / f"s{i}", float_arr.shape, float_arr.offset, float_arr.voxel_size, float_arr.axis_names, float_arr.units, dtype=np.uint8)
-    # store_rgb = zarr.open(zarr_path / "raw" / f"s{i}")
-    # # dask.array.store(dask_array, store_rgb)
-    # dask.array.store(float_arr.data, store_rgb)
+    # grab resolution in cm, convert to nm, calculate for each pyramid level
+    voxel_size = Coordinate(int(1/x_res * 1e7) * 2 ** i , int(1/y_res * 1e7) * 2 ** i)
+    # format data as funlib dataset
+    raw = prepare_ds( zarr_path / "raw" / f"s{i}", dask_array.shape, offset, voxel_size, axis_names, units, dtype=np.uint8)
+    # storage info
+    store_rgb = zarr.open(zarr_path / "raw" / f"s{i}")
+    # store info with storage info
+    dask.array.store(dask_array, store_rgb)
+    # open pyramid level and assign appropriate voxel size and units to the dataset
     ds = zarr.open(zarr_path / "raw" / f"s{i}")
     ds.attrs["voxel_size"] = voxel_size
     ds.attrs["units"] = units
-
 
 # grab new zarr with reasonable chunks as dask array
 s3_array = open_ds(zarr_path / "raw" / "s3")
@@ -81,6 +81,7 @@ fmask = prepare_ds(
 store_fgbg = zarr.open(zarr_path / "mask" / "foreground")
 dask.array.store(mask, store_fgbg)
 
+# fill holes in foreground mask and save as filled mask
 fill_hole_kernel = disk(
     15, decomposition="sequence"
 )  # sequence for computational efficiency
@@ -98,8 +99,7 @@ mask_filled_array = prepare_ds(
 )
 mask_filled_array[:] = mask_filled
 
-
-# create integral mask of foreground mask
+# create integral mask of filled foreground mask and save
 integral_mask = integral_image(mask_filled)
 imask = prepare_ds(
     zarr_path / "mask" / "integral_foreground",
@@ -131,18 +131,19 @@ patch_counts = (
 )
 patch_count_mask = (
     patch_counts >= patch_shape[0] * patch_shape[1] * 0.50
-)  # 90% of voxels are tissue
+)  # 50% of voxels are tissue
 
 # make grid on s3
 patch_spacing = Coordinate(100, 100)
 
-# very strange... why we give y shape then x shape... but it work
+# very strange... why we give y shape then x shape... but it works
 grid_x, grid_y = np.meshgrid(
     range(patch_count_mask.shape[1]), range(patch_count_mask.shape[0])
 )
 gridmask = grid_x % patch_spacing[0] + grid_y % patch_spacing[1]
 gridmask = gridmask == 0
 
+# create mask of pixels for possible starting pixels gien the ROI size and threshold
 patch_mask = prepare_ds(
     zarr_path / "mask" / "patch_mask",
     patch_count_mask.shape,
@@ -164,18 +165,21 @@ offsets = [
 
 s2_array = open_ds(zarr_path / "raw" / "s2")
 
-print(len(offsets))
 import time
 
 with torch.no_grad():
+    # load vgg16 weights
     checkpoint = torch.load(
         weights_path, map_location=torch.device("cpu"), weights_only=False
     )
+    # load vgg16 architecture
     vgg16: torch.nn.Module = torch.load(
         starting_model_path, map_location=torch.device("cpu"), weights_only=False
     )
+    # load weights into architecture
     vgg16.load_state_dict(checkpoint["model_state_dict"])
 
+    # make dataset for mask 
     pred_mask = prepare_ds(
         zarr_path / "mask" / "cortexmedulla",
         s2_array.shape[0:2],
@@ -186,24 +190,30 @@ with torch.no_grad():
         mode="w",
         dtype=np.uint8,
     )
+    # default pixel value to 2
     pred_mask[:] = 2
 
+    # for each patch, send to vgg16 and write label to mask
     for offset in tqdm(offsets):
         t1 = time.time()
+        # world units roi selection
         roi = Roi(offset, patch_size)
         voxel_roi = (roi - s2_array.offset) / s2_array.voxel_size
         patch_raw = s2_array[
             voxel_roi.begin[0] : voxel_roi.end[0], voxel_roi.begin[1] : voxel_roi.end[1]
         ]
         t2 = time.time()
+        # format roi for vgg16
         in_data = T.Resize((224, 224))(
             torch.from_numpy(patch_raw).float().permute(2, 0, 1).unsqueeze(0) / 255
         )
         t3 = time.time()
+        # predict
         pred = vgg16(in_data).squeeze(0).numpy().argmax()
         t4 = time.time()
-        context = (patch_size - patch_spacing * s3_array.voxel_size) // 2
 
+        #  write to mask, accounting for roi overlap
+        context = (patch_size - patch_spacing * s3_array.voxel_size) // 2
         slices = pred_mask._Array__slices(roi.grow(-context, -context))
         pred_mask._source_data[slices] = pred
 
